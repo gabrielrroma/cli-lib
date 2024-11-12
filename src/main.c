77b94a69493ec3fcc9188e9c26f02b9c6118d6ed
Gabriel Roma <100969556+gabrielrroma@users.noreply.github.com>
2024-11-12 17:47:37 -0300

    Update main.c

--- src/main.c
+++ src/main.c
@@ -12,3 +12,22 @@
 #define BORDER_CHAR '*'
 #define EMPTY_CHAR ' ' 
 #define SNAKE_BODY 'O'
+
+typedef struct {
+    int x, y;
+} SnakeSegment;
+
+typedef struct {
+    char name[30];
+    int score;
+} RankingEntry;
+
+RankingEntry ranking[3];
+
+SnakeSegment* snake;
+int snakeLength = 1;
+int snakeDirection = 1;
+
+int score = -10;
+
+int foodX, foodY;
