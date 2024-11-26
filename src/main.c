68f7f3eded5c31199ab7d0598675b17f9ab6dd03
Gabriel Roma <100969556+gabrielrroma@users.noreply.github.com>
2024-11-26 14:35:02 -0300

    ranking

--- src/main.c
+++ src/main.c
@@ -31,3 +31,41 @@ int snakeDirection = 1;
 int score = -10;
 
 int foodX, foodY;
+
+void loadRanking() {
+    FILE *file = fopen("ranking.dat", "rb");
+    if (file != NULL) {
+        fread(ranking, sizeof(RankingEntry), 3, file);
+        fclose(file);
+    }
+}
+
+void saveRanking() {
+    FILE *file = fopen("ranking.dat", "wb");
+    if (file != NULL) {
+        fwrite(ranking, sizeof(RankingEntry), 3, file);
+        fclose(file);
+    }
+}
+
+void addPlayerToRanking(const char *name, int score) {
+    for (int i = 0; i < 3; ++i) {
+        if (score > ranking[i].score) {
+            for (int j = 2; j > i; --j) {
+                strcpy(ranking[j].name, ranking[j - 1].name);
+                ranking[j].score = ranking[j - 1].score;
+            }
+            strcpy(ranking[i].name, name);
+            ranking[i].score = score;
+            break;
+        }
+    }
+}
+
+void showRanking() {
+    printf("\n ----- RANKING -----\n");
+    for(int i = 0; i<3; ++i) {
+        printf("%d. %s - %d\n", i + 1, ranking[i].name, ranking[i].score);
+    }
+    printf("\n");
+}
